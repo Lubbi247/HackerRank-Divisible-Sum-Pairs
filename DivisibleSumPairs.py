2702682c04a31a29b42e1703897a353b2ff5caa7
Lubabalo <lubabalo.bongweni@gmail.com>
2021-02-22 14:00:22 +0200

    Create DivisibleSumPairs.py

---
+++ DivisibleSumPairs.py
@@ -0,0 +1,8 @@
+def divisibleSumPairs(n, k, ar):
+    count=0;
+    for i in range(len(ar)-1):
+        for j in range(i+1,len(ar)) :
+            #if i>j:
+            if (ar[i]+ ar[j]) % k == 0:
+                   count += 1
+    return count
